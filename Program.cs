9397ccf58e0c861768300d302e5ffee12056e7d9
Виталий Моисеенко <88559269+QFolli@users.noreply.github.com>
2021-09-25 11:09:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Task2.Subscriber;
+using Task2.Publisher;
+
+namespace Task2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            WeatherData weatherData = new WeatherData();
+            CurrentDisplay cureentDisplay = new CurrentDisplay(weatherData);
+            StatisticDisplay statisticDisplay = new StatisticDisplay(weatherData);
+            weatherData.NotifySub();
+        }
+    }
+}
