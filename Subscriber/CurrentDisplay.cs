426f4fc05d1807fc50a39ee307d656c4d029638d
Виталий Моисеенко <88559269+QFolli@users.noreply.github.com>
2021-09-25 11:09:01 +0300

    Add files via upload

---
+++ Subscriber/CurrentDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Task2.Publisher;
+
+namespace Task2.Subscriber
+{
+    public class CurrentDisplay : ISubscriber
+    {
+        public CurrentDisplay(IPublisher publisher)
+        {
+            publisher.AddSubscriber(this);
+        }
+        public void GetNotify(double t, double p, double h)
+        {
+            Console.WriteLine(":АКТУАЛЬНАЯ ИНФОРМАЦИЯ:");
+            Console.WriteLine($"Температура: {t} град. Цельсия");
+            Console.WriteLine($"Давление: {p} мм.рт.ст");
+            Console.WriteLine($"Влажность воздуха: {h} %");
+        }
+    }
+}
---
+++ Subscriber/StatisticDisplay.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Task2.Publisher;
+
+namespace Task2.Subscriber
+{
+    class StatisticDisplay : ISubscriber
+    {
+        public StatisticDisplay(IPublisher publisher)
+        {
+            publisher.AddSubscriber(this);
+        }
+        private static List<double> tempList = new List<double>() { -5, 10, 15, 21 };
+        private static List<double> pressureList = new List<double>() { 610, 653 , 713, 780 };
+        private static List<double> humidityList = new List<double>() { 32, 43, 25, 65 };
+
+        public void GetNotify(double temp, double pressure, double humidity)
+        {
+            tempList.Add(temp);
+            pressureList.Add(pressure);
+            humidityList.Add(humidity);
+
+
+            Console.WriteLine(":СРЕДНИЕ ЗНАЧЕНИЯ ЗА СЕГОДНЯ::");
+            Console.WriteLine($"Средняя температура: {FindAverage(tempList)} град. Цельсия");
+            Console.WriteLine($"Среднее давление: {FindAverage(pressureList)} мм.рт.ст.");
+            Console.WriteLine($"Средняя влажность воздуха: {FindAverage(humidityList)} %");
+        }
+
+        private static double FindAverage(List<double> data)
+        {
+            return data.Average();
+        }
+    }
+}
